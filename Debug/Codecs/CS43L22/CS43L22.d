e91c401a749667e6afc3e1ac00e8cfcae9e62d37
embeddedalpha <kunalsalvi63@gmail.com>
2024-09-27 22:17:35 -0500

    Updated project

--- Debug/Codecs/CS43L22/CS43L22.d
+++ Debug/Codecs/CS43L22/CS43L22.d
@@ -1,1 +1,28 @@
-Codecs/CS43L22/CS43L22.o: ../Codecs/CS43L22/CS43L22.c
+Codecs/CS43L22/CS43L22.o: ../Codecs/CS43L22/CS43L22.c \
+ ../Codecs/CS43L22/CS43L22.h ../Inc/main.h ../Inc/stm32f407xx.h \
+ ../Inc/core_cm4.h ../Inc/cmsis_version.h ../Inc/cmsis_compiler.h \
+ ../Inc/cmsis_gcc.h ../Inc/mpu_armv7.h ../Inc/system_stm32f4xx.h \
+ ../Inc/stm32f4xx.h ../Codecs/CS43L22/CS43L22_Defs.h \
+ B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/GPIO/GPIO.h \
+ B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/GPIO/GPIO_Defs.h \
+ B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/DMA/DMA.h \
+ B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/DMA/DMA_Defs.h \
+ B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/I2C/I2C.h \
+ B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/I2C/I2C_Defs.h
+../Codecs/CS43L22/CS43L22.h:
+../Inc/main.h:
+../Inc/stm32f407xx.h:
+../Inc/core_cm4.h:
+../Inc/cmsis_version.h:
+../Inc/cmsis_compiler.h:
+../Inc/cmsis_gcc.h:
+../Inc/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
+../Inc/stm32f4xx.h:
+../Codecs/CS43L22/CS43L22_Defs.h:
+B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/GPIO/GPIO.h:
+B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/GPIO/GPIO_Defs.h:
+B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/DMA/DMA.h:
+B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/DMA/DMA_Defs.h:
+B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/I2C/I2C.h:
+B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/I2C/I2C_Defs.h:
--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -7,7 +7,9 @@ Src/main.o: ../Src/main.c \
  B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/GPIO/GPIO_Defs.h \
  B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/DMA/DMA.h \
  B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/DMA/DMA_Defs.h \
- B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/I2C/I2C_Defs.h
+ B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/I2C/I2C_Defs.h \
+ B:/STM32F407V_Firmware_C_Lang/Codecs/Codecs/CS43L22/CS43L22.h \
+ B:/STM32F407V_Firmware_C_Lang/Codecs/Codecs/CS43L22/CS43L22_Defs.h
 B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/I2C/I2C.h:
 ../Inc/main.h:
 ../Inc/stm32f407xx.h:
@@ -23,3 +25,5 @@ B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/GPIO/GPIO_Defs.h:
 B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/DMA/DMA.h:
 B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/DMA/DMA_Defs.h:
 B:/STM32F407V_Firmware_C_Lang/Codecs/Drivers/I2C/I2C_Defs.h:
+B:/STM32F407V_Firmware_C_Lang/Codecs/Codecs/CS43L22/CS43L22.h:
+B:/STM32F407V_Firmware_C_Lang/Codecs/Codecs/CS43L22/CS43L22_Defs.h:
