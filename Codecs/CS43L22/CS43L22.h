bbf8e4497abd0310197b7e74d53c293c46742ee3
embeddedalpha <kunalsalvi63@gmail.com>
2024-09-27 22:16:43 -0500

    Updated CS43L22.h

--- Codecs/CS43L22/CS43L22.h
+++ Codecs/CS43L22/CS43L22.h
@@ -12,11 +12,23 @@
 #include "CS43L22_Defs.h"
 #include "GPIO/GPIO.h"
 #include "DMA/DMA.h"
+#include "I2C/I2C.h"
+
+#define CS43L22_Address 0x4A
 
 
 typedef struct CS43L22_Config
 {
 	I2C_Config hardware_i2c;
+
+	uint8_t treble_corner_frequency;
+	uint8_t bass_corner_frequency;
+	uint8_t treble_gain;
+	uint8_t bass_gain;
+	uint8_t master_volume;
+	uint8_t headphone_volume_control;
+	uint8_t speaker_volume_control;
+
 
 
 }CS43L22_Config;
@@ -37,6 +49,9 @@ uint8_t CS43L22_Tone_Control_Enable(CS43L22_Config *config);
 uint8_t CS43L22_Master_Volume_Control(CS43L22_Config *config);
 uint8_t CS43L22_Headphone_Volume_Control(CS43L22_Config *config);
 uint8_t CS43L22_Speaker_Volume_Control(CS43L22_Config *config);
+
+uint8_t CS43L22_Activate(CS43L22_Config *config);
+uint8_t CS43L22_Deactivate(CS43L22_Config *config);
 
 
 #endif /* CS43L22_CS43L22_H_ */
